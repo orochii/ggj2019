ca14a952204f049dfdc98a1745d08e9c41f92881
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-01-26 11:48:44 -0600

    Proyecto entero hasta el momento.

---
+++ Javoc/Assets/scripts/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour {
+    private static List<Interactable> _interactables;
+    public static List<Interactable> Interactables {
+        get {
+            if (_interactables == null) _interactables = new List<Interactable>();
+            return _interactables;
+        }
+    }
+
+    [SerializeField] UnityEvent eventos;
+
+    public void Awake() {
+        Interactables.Add(this);
+    }
+
+    public void Interact() {
+        if (eventos != null) eventos.Invoke();
+    }
+}
---
+++ Javoc/Assets/scripts/PlayerInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] float maxDistance = 0;
+    
+    void Update() {
+        bool interact = Input.GetButtonDown("Jump");
+        if (interact) {
+            Interactable currentInteractable = GetClosest();
+            if (currentInteractable != null) {
+                currentInteractable.Interact();
+            }
+        }
+    }
+    
+    Interactable GetClosest() {
+        Interactable closest = null;
+        float closestDistance = 0;
+        foreach (Interactable i in Interactable.Interactables) {
+            float distance = (i.transform.position - transform.position).sqrMagnitude;
+            if (distance < maxDistance) {
+                if (closest == null || closestDistance > distance) {
+                    closest = i;
+                    closestDistance = distance;
+                }
+            }
+        }
+        return closest;
+    }
+}
