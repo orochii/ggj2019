3c0a66ea97f3a5ca0583755bf9612b65217d1f5d
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-01-27 13:03:48 -0600

    GameState, cuartos pasillo y easter egg listos.

---
+++ Javoc/Assets/scripts/Audio/SoundLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "soundLibrary", menuName = "Database/Create sound library")]
+public class SoundLibrary : ScriptableObject {
+    [System.Serializable]
+    public class SoundEntry {
+        public string name;
+        public AudioClip[] clips;
+    }
+
+    [SerializeField] private SoundEntry[] entries;
+
+    public AudioClip GetClipFromName(string name) {
+        foreach(SoundEntry e in entries) {
+            if (e.name.Equals(name)) {
+                int r = Random.Range(0, e.clips.Length);
+                return e.clips[r];
+            }
+        }
+        return null;
+    }
+    
+}
---
+++ Javoc/Assets/scripts/UI/InventoryUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] GameObject[] objects = new GameObject[0];
+
+    private void FixedUpdate() {
+        if (GameManager.Instance != null) {
+            for (int i = 0; i < objects.Length; i++) {
+                bool flag = GameManager.Instance.GetFlag(i);
+                objects[i].SetActive(flag);
+            }
+        }
+    }
+}
