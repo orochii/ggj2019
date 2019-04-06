1f30e3755a7162a9aa3aa7cb2fbb5f5f32545293
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-04-06 10:53:18 -0600

    Reestructured scripts folder hierarchy.

---
+++ Javoc/Assets/scripts/ColorizeObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorizeObject : MonoBehaviour
+{
+    private Material[] _allMaterials = null;
+    private Color[] _allOriginalColors = null;
+
+
+    void Awake() {
+        // Get all renderers.
+        Renderer[] allRenderers = GetComponentsInChildren<Renderer>();
+        // Get material count.
+        int matCount = 0;
+        foreach (Renderer r in allRenderers) matCount += r.materials.Length;
+        // Create materials and original colors array.
+        _allMaterials = new Material[matCount];
+        _allOriginalColors = new Color[matCount];
+        int i = 0;
+        foreach (Renderer r in allRenderers) {
+            foreach (Material m in r.materials) {
+                _allMaterials[i] = m;
+                _allOriginalColors[i] = new Color(m.color.r, m.color.g, m.color.b, m.color.a);
+                i++;
+            }
+        }
+    }
+    
+    public void Pulsate(Color c, float time, int repeat) {
+        StopAllCoroutines();
+        StartCoroutine(PulsateCoroutine(c, time, repeat));
+    }
+
+    IEnumerator PulsateCoroutine(Color c, float time, int repeat) {
+        Reset(false);
+        int leftLoops = repeat;
+        yield return new WaitForFixedUpdate();
+        while (repeat < 0 || leftLoops >= 0) {
+            float percent = 0;
+            while (percent < 1) {
+                percent += Time.fixedDeltaTime / time;
+                float v = Hermite(Mathf.PingPong(percent, 0.5f)) * 2f;
+                Color col = Color.Lerp(Color.clear, c, v);
+                Colorize(col, false);
+                yield return new WaitForFixedUpdate();
+            }
+            leftLoops--;
+        }
+    }
+
+    private float Hermite(float t) {
+        return -t * t * t * 2f + t * t * 3f;
+    }
+    
+    public void Colorize(Color c, bool stopCoroutine = true) {
+        if (stopCoroutine) StopAllCoroutines();
+        for(int i = 0; i < _allMaterials.Length; i++) {
+            Color newC = _allOriginalColors[i] + c;
+            _allMaterials[i].color = newC;
+        }
+    }
+
+    public void Reset(bool stopCoroutine = true) {
+        if (stopCoroutine) StopAllCoroutines();
+        for (int i = 0; i < _allMaterials.Length; i++) {
+            _allMaterials[i].color = _allOriginalColors[i];
+        }
+    }
+}
---
+++ Javoc/Assets/scripts/Interactables/Effects/ObjectFlash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFlash : MonoBehaviour
+{
+    [SerializeField] ColorizeObject obj;
+    [SerializeField] float time = 1;
+    [SerializeField] int repeat;
+    [SerializeField] Color color;
+    
+    public void Colorize() {
+        obj.Colorize(color);
+    }
+    public void Flash() {
+        obj.Pulsate(color, time, repeat);
+    }
+}
